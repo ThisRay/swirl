03b178654f267c1394bf3a3701d7ee9a1eebff3a
thisray <gn01697933@gmail.com>
2016-09-09 16:26:05 +0800

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -21,21 +21,20 @@ swirl_out <- function(..., skip_before=TRUE, skip_after=FALSE) {
    #  Encoding(mes) <- "UTF-8"
    #}
 
- Encoding(mes) <- "UTF-8"
+	Encoding(mes) <- "UTF-8"
   
 	test_is.BIG5 <- function(s){!is.na(iconv(iconv(s,"BIG5","UTF-8"),"UTF-8","BIG5"))}
-	if(test_is.BIG5(mes)){mes<-'999999999999999'}
-	
-	# test_is.UTF8 <- function(s){!is.na(iconv(iconv(s,"UTF-8","BIG5"),"BIG5","UTF-8"))}  
-  
+	test_is.UTF8 <- function(s){!is.na(iconv(iconv(s,"UTF-8","BIG5"),"BIG5","UTF-8"))}  
+ # if(test_is.BIG5(mes)){mes<-'999999999999999'}
  # ## windows
- # if(grepl('mingw', sessionInfo()$R.version$os)){
- #   #mes <- iconv(mes,"UTF-8","BIG5")
+  if(grepl('mingw', sessionInfo()$R.version$os)){
+    #mes <- iconv(mes,"UTF-8","BIG5")
     
- #  if(test_is.BIG5(mes)){
- #   mes <- '1'}else{
- #   mes <- '2'}
- # }
+   if(test_is.BIG5(mes)){
+    mes <- 'BIG5 BIG5 BIG5 BIG5'
+   }else if(test_is.UTF8(mes)){
+    mes <- 'UTF8 UTF8 UTF8 UTF8'}
+  }
   
   
   message(mes)
