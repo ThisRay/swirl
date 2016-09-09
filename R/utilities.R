4c1aa65ebeeb84fb52fc672a9d1e73b8e1e43164
thisray <gn01697933@gmail.com>
2016-09-09 16:06:35 +0800

    Update utilities.R

--- R/utilities.R
+++ R/utilities.R
@@ -22,11 +22,15 @@ swirl_out <- function(..., skip_before=TRUE, skip_after=FALSE) {
   # }
 
   Encoding(mes) <- "UTF-8"
+  
+	test_is.BIG5 <- function(s){!is.na(iconv(iconv(s,"BIG5","UTF-8"),"UTF-8","BIG5"))}
+	test_is.UTF8 <- function(s){!is.na(iconv(iconv(s,"UTF-8","BIG5"),"BIG5","UTF-8"))}  
+  
   ## windows
   if(grepl('mingw', sessionInfo()$R.version$os)){
     #mes <- iconv(mes,"UTF-8","BIG5")
     
-   if(mes == iconv(iconv(mes,"UTF-8","BIG5"),"BIG5","UTF-8")){
+   if(test_is.UTF8(mes))){
     mes <- '1'}else{
     mes <- '2'}
   }
